314b2ca43e0a943e821117ee650115e9aa60bd15
Matheus Ramos Kafuri <matheuskafuri@Matheuss-MacBook-Pro.local>
2022-05-09 17:40:19 -0300

    first commit

---
+++ src/store/index.ts
@@ -0,0 +1,27 @@
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+import { ICartState } from './modules/cart/types';
+
+export interface IState {
+    cart: ICartState;
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = [sagaMiddleware];
+
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(
+        applyMiddleware(...middleware),
+    )
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
